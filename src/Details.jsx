51cbf02a3beb2e6ed63927d3687bd15f93f4054d
sasa-bonta <bonta2000@mail.ru>
2024-06-11 16:21:03 +0300

    useQuery

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,35 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
+
 const Details = () => {
-  return <h2>hi!</h2>;
+  const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet);
+
+  if (results.isError) {
+    return <h2>ohno</h2>;
+  }
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <h1>{pet.name}</h1>
+      <h2>
+        {pet.animal} — {pet.breed} — {pet.city}, {pet.state}
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </h2>
+    </div>
+  );
 };
 
 export default Details;
